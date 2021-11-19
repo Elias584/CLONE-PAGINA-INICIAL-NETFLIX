e16087952589cca27c9e4baceb3bdb05da74b702
Elias584 <eliassantos584@gmail.com>
2021-11-18 21:05:43 -0300

    clone netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:false,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+function redirecionar(){
+  window.open("https://www.youtube.com/user/GenerationIron"); //abre em uma janela
+
+}
+
+function redirecionar1(){
+  window.open("https://generationiron.com/brasil/"); //abre em uma janela
+
+}
